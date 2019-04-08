0cc052f3c7267927523440df5e4ce7483fad509b
Minsu Kim <k09089@naver.com>
2019-04-08 15:39:42 +0900

    add setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='flask_restify',
+    version='0.1',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='betas',
+    author_email='thou1999@gmail.com',
+    description='',
+    install_requires=[
+        'flask',
+        'Flask-SQLAlchemy'
+    ],
+)
