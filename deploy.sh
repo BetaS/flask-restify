f8173024dd501cf7c2d4a9eab96e833db069ca8a
Minsu Kim <k09089@naver.com>
2021-07-06 13:16:35 +0900

    mskim: deploy code remove

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-python3.8 setup.py sdist
-
-FNAME=dist/`python setup.py --fullname`.tar.gz
-
-#cp $FNAME ../barn04-server/lib/flask_restify.tar.gz
-#cp $FNAME ../sharekim-server/lib/flask_restify.tar.gz
-#cp $FNAME ../nazipsa-lab-server/lib/flask_restify.tar.gz
